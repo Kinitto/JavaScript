3a2dd2e912f83d7e5fa6f997f7c027969799f923
Usuario <joaquingomezinfor@gmail.com>
2021-10-25 13:33:54 +0200

    fetch-commit

--- ajax/posts.js
+++ ajax/posts.js
@@ -3,34 +3,44 @@ window.addEventListener("load", function () {
 })
 
 function cargarDatos() {
+    
     let tablaPosts = document.getElementById("tablaPosts");
     tablaPosts.addEventListener("click",borrar)
-    const request = new XMLHttpRequest;
-    request.open('GET', 'http://localhost:3000/posts', true);
-    request.send();
-    request.addEventListener('load', function () {
-
-        if (request.status == 200) {
-
-            let datos = JSON.parse(request.responseText);
-            let contenido = document.getElementById("body");
-            contenido.innerHTML = '';
-
-            for (let i of datos) {
-
-                contenido.innerHTML += `
-                <tr>
-                    <td>${i.title}</td>
-                    <td>${i.author}</td>
-                    <td><a href="ver.html?id=${i.id}">Ver</a><a href="#" id="${i.id}" class="borrar">Borrar</a></td>
-                    
-                </tr>
-
-                ` ;
-            }
-        }
-    });
+    tbody = document.getElementById("body")
+    getPosts()
+    .then(function(posts) {
+            tbody.innerHTML = '' // borramos el contenido de la tabla
+            posts.forEach(posts => {
+                const newPost = document.createElement('tr')
+                newPost.innerHTML = `
+                <td>${posts.title}</td>
+                <td>${posts.author}</td>
+                <td><a href="ver.html?id=${posts.id}">Ver</a><a href="#" id="${posts.id}" class="borrar">Borrar</a></td>
+                   `
+                tbody.appendChild(newPost)
+              })
+          })
+          .catch(function(error) {
+            console.error(error)
+          })
 }
+
+function getPosts() {
+    return new Promise(function(resolve, reject) {
+      let peticion = new XMLHttpRequest()
+      peticion.open('GET', 'http://localhost:3000/posts')
+      peticion.send()
+      peticion.addEventListener('load', () => {
+        if (peticion.status === 200) {
+          resolve(JSON.parse(peticion.responseText))
+        } else {
+          reject("Error " + this.status + " (" + this.statusText + ") en la petición")
+        }
+      })
+      peticion.addEventListener('error', () => reject('Error en la petición HTTP'))
+    })
+  }
+
 function borrar(e) {
     if (e.target && e.target.matches("a.borrar")){
     e.preventDefault();
