c3b1953eb982041e3a8fe94e35c45894fcd03ba6
Joaquín Gómez <joaquingomezinfor@gmail.com>
2021-10-26 23:36:17 +0200

    commit

--- webStorage/script.js
+++ webStorage/script.js
@@ -1,13 +1,23 @@
 formulario.addEventListener('submit', (e) => {
     let tarea = document.getElementById("textoTarea").value;
     let formulario = document.getElementById("formulario");
+    let div = document.getElementById("div");
+
     e.preventDefault();
 
     const datos = {
         tarea: tarea,
         finished: false
     }
-    localStorage.setItem('Key', JSON.stringify(datos));
 
-    location.reload();
+    localStorage.setItem('Key', JSON.stringify(datos));
+  
+    div.innerHTML += `
+       
+    <p>${datos.tarea}</p>
+    <p>${datos.finished}</p>                  
+
+`;
+
+
 })
