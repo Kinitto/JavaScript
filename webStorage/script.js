38049547e279d46a84eed1f9e34630a87784552d
Joaquín Gómez <joaquingomezinfor@gmail.com>
2021-10-29 00:08:25 +0200

    commit2

--- webStorage/script.js
+++ webStorage/script.js
@@ -1,11 +1,18 @@
+window.addEventListener("load", function () {
+  btnBorrar();
+
+
+})
 const textoTask = document.getElementById("textoTask");
 const addTask = document.getElementById("addTask");
 const taskVacias = document.getElementById("taskVacias");
 const ul = document.querySelector("ul");
+let i = 0;
 
 addTask.addEventListener("click", (e) => {
   e.preventDefault();
-
+  i++;
+  console.log(i);
   const text = textoTask.value;
 
   if (text !== "") {
@@ -17,6 +24,13 @@ addTask.addEventListener("click", (e) => {
     li.appendChild(btnBorrar());
     ul.appendChild(li);
 
+    const tarea={
+      task:text,
+      finished:false,
+      key:'Task'+i
+    }
+    localStorage.setItem('Task'+i,JSON.stringify(tarea));
+
     textoTask.value = "";
     taskVacias.style.display = "none";
   }
@@ -27,17 +41,23 @@ function btnBorrar() {
 
   btnBorrar.textContent = "X";
   btnBorrar.className = "btnBorrar";
-
   btnBorrar.addEventListener("click", (e) => {
+
+    e.preventDefault();
+    
+    var cositas = JSON.parse(localStorage.getItem("Task1")); // updated
+    console.log(cositas)
+    cositastexto = JSON.stringify(cositas)
+    console.log(cositastexto)
+
     const item = e.target.parentElement;
     ul.removeChild(item);
-
     const items = document.querySelectorAll("li");
-
     if (items.length === 0) {
       taskVacias.style.display = "block";
     }
+    
   });
-
+  
   return btnBorrar;
 }
